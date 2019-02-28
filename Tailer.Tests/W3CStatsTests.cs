7db361ba8655661803ce82480abfc02dbdb4fe14
Joel Bennett <Jaykul@HuddledMasses.org>
2019-02-28 00:11:10 -0500

    Implement IStatistician as W3CStats
    Including tests for thresholds & statistics

---
+++ Tailer.Tests/W3CStatsTests.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Autofac.Extras.Moq;
+using NLog.LayoutRenderers.Wrappers;
+using NUnit.Framework;
+
+namespace Tailer.Tests
+{
+    public static class RandomHelper
+    {
+        private static readonly Random pick = new Random();
+
+        public static string Random(this string[] array)
+        {
+            return array.OrderBy(x => pick.Next()).First();
+        }
+    }
+
+    [TestFixture]
+    public class W3CStatsTests
+    {
+        const string template = "127.0.0.1 - {0} [{1:dd/MMM/yyyy:HH:mm:ss zzz}] \"GET {2} HTTP/1.0\" 200 100";
+        readonly string[] users = { "alex", "bob", "evan", "frank", "hannah", "james", "jill", "john", "mark", "steve", "tina", "veronica" };
+        readonly string[] urls = { "/api/user", "/report", "/user/steve", "/user/frank", "/api/posts", "/api/post/1", "/blog/1", "/blog/2", "/api/post/2", "/user/tina", "/user/veronica", "/user/alex" };
+
+        private IEnumerable<string> GetRandomLog(DateTimeOffset offset, int seconds = 120, int rate = 10)
+        {
+            for (var t = 0; t < seconds; t++)
+            {
+                for (var i = 0; i < rate; i++)
+                {
+                    yield return string.Format(template, users.Random(), offset, urls.Random());
+                }
+
+                offset = offset.AddSeconds(1);
+            }
+        }
+
+        private IEnumerable<string> GetConstantLog(int seconds = 120, int rate = 10)
+        {
+            var now = DateTimeOffset.UtcNow;
+
+            for (var t = 0; t < seconds; t++)
+            {
+                for (var i = 0; i < rate; i++)
+                {
+                    yield return string.Format(template, users[i % users.Length], now, urls[i % urls.Length]);
+                }
+
+                now = now.AddSeconds(1);
+            }
+        }
+
+        [Test]
+        public void Returns_True_Only_When_The_Threshold_Is_Exceeded()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IConfiguration>().Setup(c => c.Interval).Returns(10);
+                mock.Mock<IConfiguration>().Setup(c => c.Threshold).Returns(5);
+                mock.Mock<IConfiguration>().Setup(c => c.Window).Returns(120);
+
+                var sut = mock.Create<W3CStats>();
+                var start = DateTimeOffset.UtcNow;
+                Statistics stats;
+
+                // A full two minutes of data with exactly threshold hits:
+                var fullLog = GetRandomLog(start, 120, 5).ToArray();
+                foreach (var logLine in fullLog)
+                {
+                    if (!sut.Update(logLine)) continue;
+
+                    stats = sut.GetStatistics();
+                    Assert.LessOrEqual(stats.WindowAverage, 5);
+                }
+
+                // But if the next interval goes over:
+                fullLog = GetRandomLog(start.AddSeconds(121), 10, 6).ToArray();
+                foreach (var logLine in fullLog)
+                {
+                    if (!sut.Update(logLine)) continue;
+                    sut.GetStatistics();
+                }
+
+                stats = sut.GetStatistics();
+                Assert.Greater(stats.WindowAverage, 5);
+            }
+        }
+
+
+        [Test]
+        public void Extended_Traffic_Test()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IConfiguration>().Setup(c => c.Interval).Returns(10);
+                mock.Mock<IConfiguration>().Setup(c => c.Threshold).Returns(5);
+                mock.Mock<IConfiguration>().Setup(c => c.Window).Returns(30);
+
+                bool result = false;
+                var sut = mock.Create<W3CStats>();
+
+                Statistics stats;
+                var start = DateTimeOffset.UtcNow;
+
+                // First, fill the Window with just enough to not trip the threshold
+                var fullLog = GetRandomLog(start, 30, 5);
+
+                foreach (var logLine in fullLog)
+                {
+                    if (!sut.Update(logLine)) continue;
+
+                    stats = sut.GetStatistics();
+                    Assert.LessOrEqual(stats.WindowAverage, 5);
+                }
+
+                // Now, go over the threshold, and it will instantly trigger
+                fullLog = GetRandomLog(start.AddSeconds(30), 10, 6);
+
+                foreach (var logLine in fullLog)
+                {
+                    if (!sut.Update(logLine)) continue;
+                    stats = sut.GetStatistics();
+                }
+                // we stopped sending logs on the 10-second boundary, so we need to read it by hand
+                stats = sut.GetStatistics();
+                Assert.Greater(stats.WindowAverage, 5);
+
+                // Now go back below the threshold. It should fall down below after half the time
+                fullLog = GetRandomLog(start.AddSeconds(40), 10, 4);
+
+                foreach (var logLine in fullLog)
+                {
+                    if (!sut.Update(logLine)) continue;
+                    stats = sut.GetStatistics();
+                }
+                // we stopped sending logs on the 10-second boundary, so we need to read it by hand
+                stats = sut.GetStatistics();
+                Assert.LessOrEqual(stats.WindowAverage, 5);
+            }
+        }
+
+
+
+        [Test]
+        public void Calculates_Statistics_Correctly()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IConfiguration>().Setup(c => c.Interval).Returns(10);
+                mock.Mock<IConfiguration>().Setup(c => c.Threshold).Returns(10);
+                mock.Mock<IConfiguration>().Setup(c => c.Window).Returns(10);
+
+                var sut = mock.Create<W3CStats>();
+
+                var fullLog = GetConstantLog(sut.Configuration.Window, sut.Configuration.Threshold).ToArray();
+                foreach (var logLine in fullLog)
+                {
+                    var result = sut.Update(logLine);
+                    Assert.IsFalse(result);
+                }
+
+                // calculate the statistics
+                var statistics = sut.GetStatistics();
+
+                Assert.AreEqual(fullLog.Length, statistics.TotalRequests);
+                Assert.AreEqual(fullLog.Length / 10, statistics.RequestsPerSecond);
+
+                // In every 10 items, there should be 4 /api, 3 /user, 2 /blog and 1 /report
+                Assert.Greater(statistics.Reports["Top Sections"]["api"], statistics.Reports["Top Sections"]["user"]);
+                Assert.Greater(statistics.Reports["Top Sections"]["user"], statistics.Reports["Top Sections"]["blog"]);
+
+                // basically, there are going to be ten users with the same number of hits
+                Assert.AreEqual(statistics.Reports["Top Users"]["alex"], statistics.Reports["Top Users"]["bob"]);
+                Assert.AreEqual(statistics.Reports["Top Users"]["evan"], statistics.Reports["Top Users"]["frank"]);
+            }
+        }
+    }
+}
---
+++ Tailer/W3CStats.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace Tailer
+{
+    /// <summary>
+    /// An implementation of ILineHandler that maintains stats
+    /// </summary>
+    public class W3CStats : IStatistician
+    {
+        // cache the actual log lines for each statistics interval (clear this whenever the stats are read)
+        private readonly List<W3CLine> log = new List<W3CLine>();
+
+        // we keep the count as an array of counts, with each bucket being 'Interval' seconds
+        private DateTimeOffset last = DateTimeOffset.MinValue;
+        private int rollingIndex = 0;
+        private int hitCounter = 0;
+        private readonly int[] rollingCount;
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Create an instance using the specified configuration
+        /// </summary>
+        /// <param name="configuration">The configuration for these stats</param>
+        public W3CStats(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            // we need enough buckets to store the window in
+            // i.e. if the window is 2 minutes, and we report 10 second intervals, we need 120/10 = 12 buckets
+            rollingCount = new int[Configuration.Window / Configuration.Interval];
+        }
+
+        /// <inheritdoc />
+        public bool Update(string line)
+        {
+            bool update = false;
+            var data = new W3CLine(line);
+
+            if (last == DateTimeOffset.MinValue)
+            {
+                last = data.Time;
+            }
+
+            // Add the counter to a bucket at the end of each interval
+            // This means we only trip alerts at the end of the interval
+            if ((data.Time - last).TotalSeconds >= Configuration.Interval)
+            {
+                CalculateStatistics(log);
+                rollingIndex = (++rollingIndex) % rollingCount.Length;
+                hitCounter = 0;
+                log.Clear();
+
+                last = data.Time;
+                update = true;
+            }
+
+            hitCounter++;
+            log.Add(data);
+
+            return update;
+        }
+
+        /// <summary>
+        /// A cached copy of the latest stats that gets cleared when read
+        /// </summary>
+        private Statistics? stats = null;
+
+        /// <summary>
+        /// Calculate (and cache) the statistics on the log
+        /// </summary>
+        /// <param name="lines">A collection of <see cref="W3CLine"/></param>
+        private void CalculateStatistics(List<W3CLine> lines)
+        {
+            rollingCount[rollingIndex] = hitCounter;
+
+            var average = 0;
+
+            if (lines.Count > 0)
+            {
+                average = (int) Math.Ceiling(lines.Count / (1 + (lines.Last().Time - lines.First().Time).TotalSeconds));
+            }
+            var window = (int) Math.Ceiling((rollingCount.Sum()) / (double) Configuration.Window);
+
+            stats = new Statistics(lines.Count, average, window, window > Configuration.Threshold);
+            stats.Value.Reports.Add("Top Sections", lines.GroupBy(line => line.Request.Path)
+                                                   .OrderByDescending(g => g.Count())
+                                                   .Take(10).ToDictionary(data => data.Key, data => data.Count()));
+
+            stats.Value.Reports.Add("Top Users", lines.GroupBy(line => line.AuthenticatedUser)
+                                                .OrderByDescending(g => g.Count())
+                                                .Take(10).ToDictionary(data => data.Key, data => data.Count()));
+
+            stats.Value.Reports.Add("Top Errors", lines.Where(line => line.Status >= 400).GroupBy(line => line.Status)
+                                                 .OrderByDescending(g => g.Count())
+                                                 .Take(10).ToDictionary(data => data.Key.ToString(), data => data.Count()));
+
+            stats.Value.Reports.Add("Problem Sections", lines.Where(line => line.Status >= 400)
+                                                       .GroupBy(line => line.Request.Path)
+                                                       .OrderByDescending(g => g.Count())
+                                                       .Take(10).ToDictionary(data => data.Key.ToString(), data => data.Count()));
+
+        }
+
+        /// <inheritdoc />
+        public Statistics GetStatistics()
+        {
+            // Support calling this from elapsed log time, or wall time
+            // 1. Update() determines Interval seconds have passed and returns true
+            // 2. The caller determines we've been idle for Interval seconds and calls this method
+            //
+            // In the second case, the stats will be null, and we'll calculate based on the current log
+            if (!stats.HasValue)
+            {
+                CalculateStatistics(log);
+                log.Clear();
+            }
+
+            // Return the cached stats, and clear the cache
+            var report = stats;
+            stats = null;
+
+            Debug.Assert(report != null, nameof(report) + " != null");
+            return report.Value;
+        }
+    }
+}
